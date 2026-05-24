c491b2f802742a5edfc0bf1ab310e55a49374a52
Amir Syahrul R <amirramadhan768@gmail.com>
2026-05-24 12:47:42 +0700

    tambah

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '../dist/output.css'  // ← Path ini tergantung struktur folder
+import './output.css'  // ← Path ini tergantung struktur folder
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
